1bf592674cd2d72aba9f619fa7a83c5bec4afc74
RenatFaiz <renatf87@gmail.com>
2019-10-25 13:15:51 +0400

    Product base class created, Lombok added

---
+++ src/main/java/ru/itpark/model/Product.java
@@ -0,0 +1,48 @@
+package ru.itpark.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Collection;
+
+
+@Getter @Setter
+public class Product<T> {
+    private long id;
+    private String name;
+    private String fullName;
+    private double rating;
+    private String review;
+    private String badge;
+    private T feature;
+    private Collection<Photo> gallery;
+    class Photo{
+        Photo(String URL){
+        };
+    }
+    private String description;
+    private int price;
+
+    public Product(long id, String name, String fullName, double rating,
+                   String review, String badge, T feature,
+                   Collection<Photo> gallery, String description, int price) {
+        this.id = id;
+        this.name = name;
+        this.fullName = fullName;
+        this.rating = rating;
+        this.review = review;
+        this.badge = badge;
+        this.feature = feature;
+        this.gallery = gallery;
+        this.description = description;
+        this.price = price;
+    }
+
+    public void buy() {
+    }
+    public void compare() {
+    }
+    public void toFavorites() {
+
+    }
+}
